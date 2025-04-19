7921e7cb1aea8117ff243df150f54932b951cf36
ossama argaz <ossama2323>
2025-04-19 23:52:21 +0100

    terminer le travail sur le filtre (ALL, ACTIVE, COMPLETED ET CLEARCOMPLETED)

--- script.js
+++ script.js
@@ -29,38 +29,61 @@ document.querySelector('.icon-moon').addEventListener('click', function () {
     document.querySelector('.attribution').classList.remove('attribution-light');
 });
 
+
 // CHECK BUTTON
+let num = 0;
 document.addEventListener("click", (event) => {
     if (event.target.closest(".check-span")) {
-        event.target.firstElementChild.classList.toggle('d-none');
-        event.target.classList.toggle('check-span-checked');
-        event.target.nextElementSibling.classList.toggle('text-barre');
-    }    
-    if(event.target.classList.contains('check-span-checked')){
-        event.target.setAttribute("data-checked", true);
+        event.target.nextElementSibling.classList.remove('d-none');
+        event.target.classList.add('d-none');
+        event.target.nextElementSibling.nextElementSibling.classList.add('text-barre');
+        event.target.nextElementSibling.setAttribute("data-checked", true);
+
         for(let i=0; i<todoArray.length; i++){
             if(todoArray[i].id == event.target.getAttribute("todo-id")){
                 todoArray[i].completed = true;
             }
         }
-    }else{
+
+        num--;
+        document.querySelector('.todo-number').innerHTML = num;
+    }    
+    if(event.target.classList.contains('check-span-checked')){
         event.target.setAttribute("data-checked", false);
         for(let i=0; i<todoArray.length; i++){
             if(todoArray[i].id == event.target.getAttribute("todo-id")){
                 todoArray[i].completed = false;
             }
         }
+        event.target.previousElementSibling.classList.remove('d-none');
+        event.target.classList.add('d-none');
+        event.target.nextElementSibling.classList.remove('text-barre');
+
+
+        num++;
+        document.querySelector('.todo-number').innerHTML = num;
     }
-    // console.log(todoArray);
-    // console.log(event.target);
+    console.log(todoArray);
+    console.log(event.target);
 });
+
 
 document.addEventListener("click", (event) => {
     if (event.target.closest(".check-img")) {
-        event.target.classList.add('d-none');
-        event.target.parentElement.classList.remove('check-span-checked');
-        event.target.parentElement.nextElementSibling.classList.remove('text-barre')
+        event.target.parentElement.classList.add('d-none');
+        event.target.parentElement.previousElementSibling.classList.remove('d-none');
+        event.target.parentElement.nextElementSibling.classList.remove('text-barre');
+
+        for(let i=0; i<todoArray.length; i++){
+            if(todoArray[i].id == event.target.getAttribute("todo-id")){
+                todoArray[i].completed = false;
+            }
+        }
+
+        num++;
+        document.querySelector('.todo-number').innerHTML = num;    
     }
+    // console.log( event.target.parentElement.nextElementSibling);
 });
 
 
@@ -73,12 +96,17 @@ let input = document.querySelector('.todo-input input');
 let id = 0;
 input.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
-        let todoText = { id: `${id++}`, text: `${input.value}`, completed: false ,order: `` };
+        if(input.value === ""){
+            return
+        }
+        let todoText = { id: `${id++}`, text: `${input.value}`, completed: false , order: `` };
 
         todoArray.push(todoText)
         view();
         input.value = '';
         // console.log(todoArray);
+        num++;
+        document.querySelector('.todo-number').innerHTML = num;
     }
 });
 
@@ -88,7 +116,8 @@ function view() {
     for (let i = 0; i < todoArray.length; i++) {
         todoArray[i].order = i;
         resultat += `<div class="todo-list-item pt-3 pb-3 d-flex align-items-center position-relative">
-                        <span class="check-span d-flex justify-content-center align-items-center rounded-circle" todo-id="${todoArray[i].order}" data-checked="${todoArray[i].completed}"><img src="./images/icon-check.svg" class="check-img d-none"></span>
+                        <span class="check-span d-flex justify-content-center align-items-center rounded-circle" todo-id="${todoArray[i].order}" data-checked="${todoArray[i].completed}"></span>
+                        <span class="check-span-checked d-flex justify-content-center align-items-center rounded-circle d-none" todo-id="${todoArray[i].order}" data-checked="${todoArray[i].completed}"><img src="./images/icon-check.svg" class="check-img"></span>
                         <span class="todo-list-text ps-3">${todoArray[i].text}</span>
                         <img class="remove-icon" todo-id="${todoArray[i].order}" src="./images/icon-cross.svg">
                     </div>`
@@ -118,7 +147,7 @@ completed.addEventListener('click', () =>{
         }
     }
     viewCompleted();
-    console.log(completedArray)
+    // console.log(completedArray)
 
 
 
@@ -127,7 +156,8 @@ function viewCompleted() {
     for (let i = 0; i < completedArray.length; i++) {
         completedArray[i].order = i;
         resultat += `<div class="todo-list-item pt-3 pb-3 d-flex align-items-center position-relative">
-                        <span class="check-span d-flex justify-content-center align-items-center rounded-circle" todo-id="${todoArray[i].order}" data-checked="${completedArray[i].completed}"><img src="./images/icon-check.svg" class="check-img d-none"></span>
+                        <span class="check-span d-flex justify-content-center align-items-center rounded-circle" todo-id="${todoArray[i].order}" data-checked="${todoArray[i].completed}"></span>
+                        <span class="check-span-checked d-flex justify-content-center align-items-center rounded-circle d-none" todo-id="${todoArray[i].order}" data-checked="${todoArray[i].completed}"><img src="./images/icon-check.svg" class="check-img"></span>                        
                         <span class="todo-list-text ps-3">${completedArray[i].text}</span>
                         <img class="remove-icon" todo-id="${completedArray[i].order}" src="./images/icon-cross.svg">
                     </div>`
@@ -173,7 +203,8 @@ active.addEventListener('click', ()=>{
         resultat = ``;
         for(let i=0; i<activeTodo.length; i++){
             resultat += `<div class="todo-list-item pt-3 pb-3 d-flex align-items-center position-relative">
-                            <span class="check-span d-flex justify-content-center align-items-center rounded-circle" todo-id="${activeTodo[i].order}" data-checked="${activeTodo[i].completed}"><img src="./images/icon-check.svg" class="check-img d-none"></span>
+                            <span class="check-span d-flex justify-content-center align-items-center rounded-circle" todo-id="${todoArray[i].order}" data-checked="${todoArray[i].completed}"></span>
+                            <span class="check-span-checked d-flex justify-content-center align-items-center rounded-circle d-none" todo-id="${todoArray[i].order}" data-checked="${todoArray[i].completed}"><img src="./images/icon-check.svg" class="check-img"></span>                            
                             <span class="todo-list-text ps-3">${activeTodo[i].text}</span>
                             <img class="remove-icon" todo-id="${activeTodo[i].order}" src="./images/icon-cross.svg">
                         </div>` 
@@ -189,3 +220,21 @@ active.addEventListener('click', ()=>{
         }
     })
 });
+
+
+
+// CLEAR COMPLETED
+
+let clear = document.querySelector('.clear');
+let toRemove = [];
+clear.addEventListener('click', ()=>{
+    for(let i=0; i<todoArray.length; i++){
+        if(todoArray[i].completed){
+            toRemove.push(todoArray[i])
+        }
+    }
+    todoArray = todoArray.filter(todo => !toRemove.includes(todo))
+    view()
+    console.log(todoArray);
+    console.log(toRemove);
+})
